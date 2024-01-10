37d29eb0c27f04e317aca6f2ae97a44edb6128fb
sukebenedek <suke.benedek@students.jedlik.eu>
2024-01-10 16:24:05 +0100

    animacio

--- script/key.js
+++ script/key.js
@@ -4,14 +4,19 @@ let ySpeed = 15
 let canLeftJump = true
 let canRightJump = true
 
+let leftRun = false
+let rightRun = false
+
 addEventListener("keydown", function(e){
     if (e.code == 'KeyD'){
         LspeedXRight = xSpeed;
         leftLastDir = "Right"
+        leftRun = true
     } 
     if (e.code == 'KeyA'){
         LspeedXLeft = -xSpeed;
         leftLastDir = "Left"  
+        leftRun = true
     }
     if (e.code == 'KeyS') LspeedY = 10;
     if (e.code == 'KeyW'){
@@ -24,8 +29,14 @@ addEventListener("keydown", function(e){
 })
 
 addEventListener("keyup", function(e){
-    if (e.code == 'KeyD') LspeedXRight = 0;
-    if (e.code == 'KeyA') LspeedXLeft = 0;
+    if (e.code == 'KeyD'){
+        LspeedXRight = 0;
+        leftRun = false
+    } 
+    if (e.code == 'KeyA'){
+        LspeedXLeft = 0;
+        leftRun = false
+    }
     if (e.code == 'KeyS') LspeedY = 0;
     // if (e.code == 'KeyW') LspeedY = 0;
 })
@@ -34,10 +45,12 @@ addEventListener("keydown", function(e){
     if (e.code == 'ArrowRight'){
         RspeedXRight = xSpeed;
         rightLastDir = "Right"
+        rightRun = true
     } 
     if (e.code == 'ArrowLeft'){
        RspeedXLeft = -xSpeed; 
        rightLastDir = "Left"
+       rightRun = true
     } 
     if (e.code == 'ArrowDown') RspeedY = 10;
     if (e.code == 'ArrowUp'){
@@ -50,8 +63,14 @@ addEventListener("keydown", function(e){
 })
 
 addEventListener("keyup", function(e){
-    if (e.code == 'ArrowRight') RspeedXRight = 0;
-    if (e.code == 'ArrowLeft') RspeedXLeft = 0;
+    if (e.code == 'ArrowRight'){
+        RspeedXRight = 0;
+        rightRun = false
+    }
+    if (e.code == 'ArrowLeft'){
+        RspeedXLeft = 0;
+        rightRun = false
+    }
     if (e.code == 'ArrowDown') RspeedY = 0;
     // if (e.code == 'ArrowUp') RspeedY = 0;
 })
--- slo.js
+++ slo.js
@@ -46,7 +46,7 @@ context.fillStyle = 'white';
 context.fillRect(0, 0, width, height);
 
 function generatePlatform() {
-    return {posX: random(0, width - 150), posY: random(160, height - 200), width: 150, height: 50};
+    return {posX: random(0, width - 150), posY: random(160, height - 200), width: random(150, 220), height: random(50, 70), type: random(0, 3), isFlipped: Math.random() >= 0.5};
 }
 
 function checkProximity(platform, existingPlatforms, minDistance) {
@@ -82,6 +82,22 @@ let bulletHeight = 10
 let playerLeft = {posX: LpositionX, posY: LpositionY, width: BaseWidth, height: BaseHeight} //wsad, f
 let playerRight = {posX: RpositionX, posY: RpositionY, width: BaseWidth, height: BaseHeight} //nyilak, ctrl
 
+let leftPlayerImage = document.getElementById("leftPlayerImage");
+let leftPlayerImageLeft = document.getElementById("leftPlayerImageLeft");
+let leftPlayerImageRight = document.getElementById("leftPlayerImageRight");
+
+let rightPlayerImage = document.getElementById("rightPlayerImage");
+let rightPlayerImageLeft = document.getElementById("rightPlayerImageLeft");
+let rightPlayerImageRight = document.getElementById("rightPlayerImageRight");
+
+let platform0 = document.getElementById("platform0");
+let platform1 = document.getElementById("platform1");
+let platform2 = document.getElementById("platform2");
+
+let platformImgs = [platform0, platform1, platform2]
+
+let framesBetweenAnimations = 20;
+let frames = 0
 
 function move(){
     playerLeft = {posX: LpositionX, posY: LpositionY, width: BaseWidth, height: BaseHeight} //wsad, f
@@ -89,19 +105,86 @@ function move(){
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
     
-
-    context.fillStyle = 'blue';
-    context.fillRect(playerLeft.posX, playerLeft.posY, playerLeft.width, playerLeft.height);
-
-    context.fillStyle = 'red';
-    context.fillRect(playerRight.posX, playerRight.posY, playerRight.width, playerRight.height);
-
     for (let i = 0; i < allPlatforms.length; i++) {
         const element = allPlatforms[i];
-
+    
         drawPlatform(element)
-
+    
     }
+    
+    frames++
+    if(leftRun){
+        if (leftLastDir == "Right") {
+            if (Math.round(frames / framesBetweenAnimations) % 2) {
+                context.drawImage(leftPlayerImageLeft, playerLeft.posX, playerLeft.posY, playerLeft.width, playerLeft.height);
+            } else {
+                context.drawImage(leftPlayerImageRight, playerLeft.posX, playerLeft.posY, playerLeft.width, playerLeft.height);
+            }
+        } else {
+            context.save();
+            context.scale(-1, 1);
+        
+            if (Math.round(frames / framesBetweenAnimations) % 2) {
+                context.drawImage(leftPlayerImageLeft, -playerLeft.posX - playerLeft.width, playerLeft.posY, playerLeft.width, playerLeft.height);
+            } else {
+                context.drawImage(leftPlayerImageRight, -playerLeft.posX - playerLeft.width, playerLeft.posY, playerLeft.width, playerLeft.height);
+            }
+        
+            context.restore();
+        }
+        
+        
+    }
+    else{
+        if (leftLastDir == "Right") {
+            context.drawImage(leftPlayerImage, playerLeft.posX, playerLeft.posY, playerLeft.width, playerLeft.height);
+        }
+        else{
+            context.save();
+            context.scale(-1, 1);
+            context.drawImage(leftPlayerImage, -playerLeft.posX - playerLeft.width, playerLeft.posY, playerLeft.width, playerLeft.height);
+
+            context.restore();
+
+        }
+    }
+
+    if(rightRun){
+        if (rightLastDir == "Right") {
+            if (Math.round(frames / framesBetweenAnimations) % 2) {
+                context.drawImage(rightPlayerImageLeft, playerRight.posX, playerRight.posY, playerRight.width, playerRight.height);
+            } else {
+                context.drawImage(rightPlayerImageRight, playerRight.posX, playerRight.posY, playerRight.width, playerRight.height);
+            }
+        } else {
+            context.save();
+            context.scale(-1, 1);
+        
+            if (Math.round(frames / framesBetweenAnimations) % 2) {
+                context.drawImage(rightPlayerImageLeft, -playerRight.posX - playerRight.width, playerRight.posY, playerRight.width, playerRight.height);
+            } else {
+                context.drawImage(rightPlayerImageRight, -playerRight.posX - playerRight.width, playerRight.posY, playerRight.width, playerRight.height);
+            }
+        
+            context.restore();
+        }
+        
+        
+    }
+    else{
+        if (rightLastDir == "Right") {
+            context.drawImage(rightPlayerImage, playerRight.posX, playerRight.posY, playerRight.width, playerRight.height);
+        }
+        else{
+            context.save();
+            context.scale(-1, 1);
+            context.drawImage(rightPlayerImage, -playerRight.posX - playerRight.width, playerRight.posY, playerRight.width, playerRight.height);
+
+            context.restore();
+
+        }
+    }
+
 
     for (let i = 0; i < leftsBullets.length; i++) {
         const element = leftsBullets[i];
@@ -135,7 +218,7 @@ function move(){
             context.fillStyle = 'black';
             context.textAlign = 'center';
             context.font = '100px Comic Sans MS, sans-serif';
-            context.fillText('A job oldali játékos meghalt.', canvas.width / 2, canvas.height / 2);
+            context.fillText('A bal oldali játékos nyert.', canvas.width / 2, canvas.height / 2);
         }
        
     }
@@ -172,7 +255,7 @@ function move(){
             context.fillStyle = 'black';
             context.textAlign = 'center';
             context.font = '100px Comic Sans MS, sans-serif';
-            context.fillText('A bal oldali játékos meghalt.', canvas.width / 2, canvas.height / 2);
+            context.fillText('A jobb oldali játékos nyert.', canvas.width / 2, canvas.height / 2);
         }
        
     }
@@ -219,9 +302,21 @@ function move(){
 
 }
 
+
+
 function drawPlatform(platform){
-    context.fillStyle = 'orange';
-    context.fillRect(platform.posX, platform.posY, platform.width, platform.height);
+    // context.fillStyle = 'orange';
+    // context.fillRect(platform.posX, platform.posY, platform.width, platform.height);
+    if(!platform.isFlipped){
+        context.drawImage(platformImgs[platform.type], platform.posX, platform.posY, platform.width, platform.height);
+    }
+    else{
+        context.save();
+        context.scale(-1, 1);
+        context.drawImage(platformImgs[platform.type], -platform.posX - platform.width, platform.posY, platform.width, platform.height);
+        context.restore();
+    }
+
 }
 
 function drawBullet(bullet){
@@ -289,9 +384,11 @@ function Limits(){
 }
 
 
+let gunPos = 77
+
 function ShootLeft(){
     if(canLeftShoot){
-        let currentLeftBullet = {posX: playerLeft.posX, posY: playerLeft.posY + 35, width: bulletWidth, height: bulletHeight, direction: leftLastDir}
+        let currentLeftBullet = {posX: playerLeft.posX, posY: playerLeft.posY + gunPos, width: bulletWidth, height: bulletHeight, direction: leftLastDir}
         leftsBullets.push(currentLeftBullet)
         canLeftShoot = false
 
@@ -305,7 +402,7 @@ function ShootLeft(){
 
 function ShootRight(){
     if(canRightShoot){
-        let currentRightBullet = {posX: playerRight.posX, posY: playerRight.posY + 35, width: bulletWidth, height: bulletHeight, direction: rightLastDir}
+        let currentRightBullet = {posX: playerRight.posX, posY: playerRight.posY + gunPos, width: bulletWidth, height: bulletHeight, direction: rightLastDir}
         rightsBullets.push(currentRightBullet)
         canRightShoot = false
 
